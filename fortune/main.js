eb517070be49f25b582b6b4e36044070298e6878
Liuhuaying <irenes2007@163.com>
2017-02-23 22:38:55 -0500

    added animation and transition

--- fortune/main.js
+++ fortune/main.js
@@ -1,38 +1,44 @@
 
 //init canvas
-var m = {t:30,r:20,b:50,l:50},
-    w = document.getElementById('canvas').clientWidth - m.l - m.r,
-    h = document.getElementById('canvas').clientHeight - m.t - m.b;
+var m = {t:30,r:20,b:50,l:50}
+    // w = document.getElementById('canvas').clientWidth - m.l - m.r,
+    // h = document.getElementById('canvas').clientHeight - m.t - m.b;
 
 var svg = d3.select('#canvas')
     .append('svg')
-    .attr('width', w + m.l + m.r)
-    .attr('height', h + m.t + m.b)
-    // .attr('preserveAspectRatio','none')
+    // .attr('width', w + m.l + m.r)
+    // .attr('height', h + m.t + m.b)
     plot=svg.append('g').attr('class','plot')
     .attr('transform','translate('+ m.l+','+ m.t+')');
 
 //set scales
 var scaleColor = d3.scaleOrdinal()
 	.domain(['China','Taiwan','Hongkong'])
-	.range(['#A3FB66','#D65671','#2BA3E6']),
+	// .range(['#A3FB66','#D65671','#2BA3E6']),
+    .range(['#A3FB66','#F95392','#08C5F7']),
 	scaleX = d3.scaleBand()
-	.range([w/22,w])
+	// .range([w/22,w])
     .paddingOuter(.3)
     .paddingInner(.6),
 	scaleY = d3.scaleLinear()
 	.domain([500,1])
-	.range([h,h/500+h*.15]);
+	
 var lineGenerator = d3.line()
     .x(function(d){return scaleX(d.year)})
     .y(function(d){return scaleY(d.rank)})
     .curve(d3.curveCardinal.tension(0.5));
 var axisX = d3.axisBottom().tickPadding(10),
     axisY = d3.axisLeft().tickPadding(10);
-var rankbg,X,Y,years,isFirst;
+var rankbg,X,Y,years,isFirst,rankYear;
 var Byname = JSON.parse(localStorage.getItem('Byname')) || [];
 var fortune = JSON.parse(localStorage.getItem('fortune')) || [];
-
+var keys = [];
+    keys.push({country:'China'});
+    keys.push({country:'Hongkong'});
+    keys.push({country:'Taiwan'});
+    rankbg = plot.append('g').attr('class','diagram');
+    X = rankbg.append('g').attr('class','axis axis-x')
+    Y = rankbg.append('g').attr('class','axis axis-y');
 //add animation to lines
 function setanimationline(){
     var paths = document.querySelectorAll('.country');
@@ -48,14 +54,14 @@ function animationline(path){
     path.style.strokeDasharray = length+' ' + length;
     path.style.strokeDashoffset = length;
     path.getBoundingClientRect();
-    path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 3s ease-in-out';
+    path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 5s ease-in-out';
     path.style.strokeDashoffset = '0';
 }
 
 
 d3.csv('../data/fortunedata_01252017.csv',parse,dataloaded);
 function dataloaded(err, fortune){
-// console.table(fortune);
+console.table(fortune);
 Byname = d3.nest()
     .key(function(d){return d.name}).sortKeys(d3.ascending)
     .rollup(function(leaves){
@@ -70,17 +76,23 @@ Byname = d3.nest()
         }
     })
     .entries(fortune)
-localStorage.setItem('Byname', JSON.stringify(Byname));
+
+localStorage.setItem('Byname', JSON.stringify(Byname))
 localStorage.setItem('fortune', JSON.stringify(fortune));
 years = d3.map(fortune,function(d){return d.year})
 
 scaleX.domain(years.keys())
 axisX.scale(scaleX).tickValues(years.keys()).tickSize(0);
-axisY.scale(scaleY).tickValues([1,100,200,300,400,500]).tickSize(-w);
+axisY.scale(scaleY).tickValues([1,100,200,300,400,500])
+// .tickSize(-w);
 
-
+isFirst=false;
+setsight()
 creatediagram()
-dots(fortune);lines(Byname);
+
+dots(fortune);setTimeout(function(){lines(Byname)}, 1500);
+
+
 
 window.addEventListener('scroll', debounce(checkSlide));
 
@@ -89,7 +101,7 @@ window.addEventListener('scroll', debounce(checkSlide));
 function checkSlide(e) {
     const slides = document.querySelectorAll('.slide');
     slides.forEach(slide => {
-        console.log(slide.offsetTop,slide.clientHeight)
+        // console.log(slide.offsetTop,slide.clientHeight)
 
         // half way through the slide
         const slideInAt = (window.scrollY + window.innerHeight) - slide.clientHeight/2;
@@ -98,12 +110,15 @@ function checkSlide(e) {
         const isHalfShown = slideInAt > slide.offsetTop;
         const isNotScrolledPast = window.scrollY < slideBottom;
         if (isHalfShown && isNotScrolledPast) {
-         console.log(slide.id+' :i am in');
-         switch(slide.id){  
+         // console.log(slide.id+' :i am in');
+         switch(slide.id){
+            case 'slide0':
+                d3.selectAll('.country').style('opacity',.9);
+                break;
             case 'slide1': 
                 isFirst=true;
                 setsight();
-                plot.selectAll('.country').style('opacity',0);
+                d3.selectAll('.country').style('opacity',0);
                 break;
             case 'slide2':
                 resetline('.China');
@@ -143,13 +158,15 @@ function checkSlide(e) {
                 break;
          }
         } else {
-          console.log(slide.id+' :i am out');
+          // console.log(slide.id+' :i am out');
           switch(slide.id){
+            case 'slide0':
+
             case 'slide1':           
-               if(isNotScrolledPast){isFirst=false;setsight();}
+               if(isNotScrolledPast){isFirst=false;setsight();d3.selectAll('.country').style('opacity',.9);}
                 break;
             case 'slide2':            
-                if(isNotScrolledPast){plot.selectAll('.country').style('opacity',0)}
+                if(isNotScrolledPast){plot.selectAll('.country').style('opacity',0);d3.selectAll('.dots').style('opacity',.7)}
 
                 break;
             case 'slide3':
@@ -189,59 +206,49 @@ function checkSlide(e) {
       })
 }
 function setsight(){
+
     if(isFirst){
         document.querySelector('#canvas').classList.remove('blur'); 
         document.querySelector('#diagram').classList.remove('col-lg-12', 'col-md-12', 'col-sm-12')
         document.querySelector('#diagram').classList.add('col-lg-8', 'col-md-8', 'col-sm-8')
-        w = document.getElementById('canvas').clientWidth - m.l - m.r,
-        h = document.getElementById('canvas').clientHeight - m.t - m.b;
-        svg.attr('width', w + m.l + m.r).attr('height', h + m.t + m.b)
-        scaleX.range([w/22,w])
-        d3.select('.diagram').remove();
-        creatediagram()
-        dots(fortune);lines(Byname);
+        lines(Byname);
         isFirst=false;
     }
     else{
         document.querySelector('#canvas').classList.add('blur'); 
         document.querySelector('#diagram').classList.remove('col-lg-8', 'col-md-8', 'col-sm-8')
-        document.querySelector('#diagram').classList.add('col-lg-12', 'col-md-12', 'col-sm-12')
-        w = document.getElementById('canvas').clientWidth - m.l - m.r,
-        h = document.getElementById('canvas').clientHeight - m.t - m.b;
-        svg.attr('width', w + m.l + m.r).attr('height', h + m.t + m.b)
-        scaleX.range([w/22,w])
-        d3.select('.diagram').remove();
-        creatediagram()
-        dots(fortune);lines(Byname);
-        isFirst=true
+        document.querySelector('#diagram').classList.add('col-lg-12', 'col-md-12', 'col-sm-12')  
+        lines(Byname);resetline('.country')
+        isFirst=true;
     }
+    w = document.getElementById('canvas').clientWidth - m.l - m.r,
+    h = document.getElementById('canvas').clientHeight - m.t - m.b;
+    svg.attr('width', w + m.l + m.r).attr('height', h + m.t + m.b)
+    scaleX.range([w/22,w])
+    axisY.tickSize(-w);
+    scaleY.range([h,h/500+h*.15]);
+    setTimeout(function(){dots(fortune);}, 500);
+    X.transition().call(axisX);    
 }
 function creatediagram(){
-
-    rankbg = plot.append('g').attr('class','diagram');
-    X = rankbg.append('g').attr('class','axis axis-x').attr('transform','translate(0,'+ h +')'),
-    Y = rankbg.append('g').attr('class','axis axis-y');
+    X.attr('transform','translate(0,'+ h +')'),
     X.call(axisX);
     Y.call(axisY);
     rankbg.append('g').attr('class','circle');
     rankbg.append('g').attr('class','line');
     d3.selectAll('.tagprofit').remove();
     rankbg.append("text").attr('class','tagrank')
-        .attr("transform", "translate(" + (w/2) + "," + (h + m.b/1.3) + ")")
         .style("text-anchor", "middle")
-        .text("Year");
+        .text("Year")
+        .attr("transform", "translate(" + (w/4) + "," + (h + m.b/1.3) + ")")
     rankbg.append("text").attr('class','tagrank')
         .attr("transform", "translate(" + (-m.l/4) + "," + (m.t*2) + ")")
         .style("text-anchor", "middle")
         .text("Rank");
-    var keys = [];
-    keys.push({country:'China'});
-    keys.push({country:'Hongkong'});
-    keys.push({country:'Taiwan'});
     var key=rankbg.selectAll('.key')
         .data(keys).enter()
         .append("g").attr('class','key')
-        .attr("transform", function(d,i){return 'translate(' + (i + 1) * (w/4) + ',' + (m.t) + ')'})
+        .attr("transform", function(d,i){return 'translate(' + (i + 1) * (w/8) + ',' + (m.t) + ')'})
 
     key.append('circle')
         .attr('r',6)
@@ -257,24 +264,34 @@ function creatediagram(){
 //to select specific country/industry's lines
 function resetline(name,filter){
     // console.log(name.substr(1))
+     var getchange=d3.map(Byname,function(d){return d.key})
     if(name != '.country'){
-        plot.selectAll('.dots').style('opacity',.3);
+        plot.selectAll('.dots').style('opacity',.2);
 
-        var getchange=d3.map(Byname,function(d){return d.key})
+       
         plot.selectAll('.dots').filter(d=>{
             // console.log(d)
+
             var itschange=getchange.get(d.name)
+            // console.log(itschange.value.values[itschange.value.values.length-1])
+            // console.log(d.year)
             if(filter == undefined){
-                return d.location == name.substr(1);
+                return d.location == name.substr(1) && (d.year == itschange.value.values[itschange.value.values.length-1].year || d.year == itschange.value.values[0].year);
             }
             else{
                 // console.log(d.change+":"+itschange.value.change)
-                return d.change == "" ? d.location == name.substr(1) && itschange.value.change == filter : d.location == name.substr(1) && d.change == filter
+                return (d.change == "" ? d.location == name.substr(1) && itschange.value.change == filter : d.location == name.substr(1) && d.change == filter) && (d.year == itschange.value.values[itschange.value.values.length-1].year || d.year == itschange.value.values[0].year)
             }
         })
         .style('opacity',.9)  
     }
-    else{plot.selectAll('.dots').style('opacity',.9)}
+    else{
+        plot.selectAll('.dots').filter(d=>{
+            var itschange=getchange.get(d.name)
+            return d.year == itschange.value.values[itschange.value.values.length-1].year || d.year == itschange.value.values[0].year;
+        })
+        .style('opacity',.9)
+    }
 
     plot.selectAll('.country').transition().style('opacity',0);
     plot.selectAll(name).filter(d=>{
@@ -314,7 +331,7 @@ function resetline(name,filter){
             break;
         }
     })
-    .transition().style('opacity',.5)   
+    .transition().style('opacity',.8)   
 }
 
 //to draw line
@@ -330,7 +347,7 @@ function lines(data){
     updateline.merge(enterline)
             .style('stroke',function(d){return scaleColor(d[0].location)})
             .style('fill','none')
-            .style('opacity',.5)
+            .style('opacity',.9)
             .style('stroke-width',2)
             
     updateline.exit().remove(); 
@@ -344,22 +361,20 @@ var updatedots = d3.select('.circle').selectAll('.dots')
 var enterdots = updatedots.enter()
         .append('circle').attr('class',function(d){return 'dots y' + d.year})
         .attr('cx',0)
-        .attr('cy',0)
+        .attr('cy',0)      
         .style('stroke',function(d){return scaleColor(d.location)})
         .style('stroke-width','2px')
         .style('fill','none')
-        .style('opacity',0)
+        .style('opacity',.7)
         .attr('r',0)
         .on('click',function(d){console.log(d.rank)})
-
+        .attr('r','4px')
+    
 updatedots.merge(enterdots)
-    // .transition().duration(2000)
-    .attr('cx',function(d){return scaleX(d.year)})
     .attr('cy',function(d){return scaleY(d.rank)})
-    .style('opacity',.5)
-    .attr('r','4px')
+    .transition()
+    .attr('cx',function(d){return scaleX(d.year)})
 
-updatedots.exit().remove(); 
 }
 
 function parse(d){
